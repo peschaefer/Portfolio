a7a5dcdd3cae970dc80b3bee496615ad68903b46
peschaefer <peschaefer@bsu.edu>
2022-07-29 20:58:29 -0400

    Change Kachirp Link to Actual GitHub Repository

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
             <h2 className={"subheader"}>Projects</h2>
             <hr className={"subline"}/>
             <div className={"project-box"}>
-                <Project name="Kachirp" url="peytonschaefer.com" image={kachirpImage} imageAlt="A question screen from Kachirp"
+                <Project name="Kachirp" url="https://github.com/peschaefer/Kachirp" image={kachirpImage} imageAlt="A question screen from Kachirp"
                 description="This project was really really cool and I am
                 going to type a lot to mess with text wrap this will all
                 change later dont even worry about it. type yeppepepepe pepewpoui ffsdhdhbighcvju
