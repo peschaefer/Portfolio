f30fc9112e9887721cde11744b70a5ee3d86dfb8
peschaefer <peschaefer@bsu.edu>
2022-07-31 01:44:07 -0400

    Add Resume and Embed into Resume Page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "../components/Header";
+import resumePDF from "../resume/resume.pdf"
 
 const Resume = () => {
     return (
         <div className="App">
             <title>Resume - PeytonSchaefer</title>
             <Header/>
+            <iframe className={"resume-pdf-viewer"} title="Resume" src={resumePDF} width="75%" height={"1150px"}>
+            </iframe>
         </div>
     );
 };
